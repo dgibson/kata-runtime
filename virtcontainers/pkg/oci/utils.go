kata-containers/runtime

device: Allow VFIO devices to be presented to guest as VFIO devices
On a conventional (e.g. runc) container, passing in a VFIO group device,
/dev/vfio/NN, will result in the same VFIO group device being available
within the container.

With Kata, however, the VFIO device will be bound to the guest
kernel's driver (if available), possibly appearing as some other
device within the guest.

This adds a new option `vfio_in_guest` which makes this behaviour
difference explicit.  If set to false, it keeps the current Kata
behaviour of binding VFIO devices to the guest native driver.  If set
to true it will instruct the agent to remap VFIO devices to the VFIO
driver within the guest as well, meaning they will appear as VFIO
devices within the container.

Unlike a runc container, the VFIO devices will have different names to the
host, since the names correspond to the IOMMU groups of the guest and those
can't be remapped with namespaces.

For now we keep 'false' as the default value, to maintain current Kata
behaviour.  In future we should change this to 'true', so that Kata's
default behaviour more closely resembles OCI specified behaviour.

fixes #2938

Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

virtcontainers/pkg/oci/utils.go
@@ -126,6 +126,10 @@ type RuntimeConfig struct {
 	//Determines if containers are allowed to join the pid namespace of the kata agent
 	EnableAgentPidNs bool
 
+	//Determines if VFIO devices get bound to the VFIO driver in
+	//the guest, or to the guest's native drivers
+	VFIOInGuest bool
+
 	//Experimental features enabled
 	Experimental []exp.Feature
 }
@@ -863,6 +867,8 @@ func SandboxConfig(ocispec specs.Spec, runtime RuntimeConfig, bundlePath, cid, c
 		EnableAgentPidNs: runtime.EnableAgentPidNs,
 
 		DisableGuestSeccomp: runtime.DisableGuestSeccomp,
+
+		VFIOInGuest: runtime.VFIOInGuest,
 
 		// Q: Is this really necessary? @weizhang555
 		// Spec: &ocispec,

dgibson/kata-runtime

virtcontainers/pkg/oci/utils.go
@@ -127,6 +127,10 @@ type RuntimeConfig struct {
 	//Determines if containers are allowed to join the pid namespace of the kata agent
 	EnableAgentPidNs bool
 
+	//Determines if VFIO devices get bound to the VFIO driver in
+	//the guest, or to the guest's native drivers
+	VFIOInGuest bool
+
 	//Experimental features enabled
 	Experimental []exp.Feature
 }
@@ -943,6 +947,8 @@ func SandboxConfig(ocispec specs.Spec, runtimeConfig RuntimeConfig, bundlePath, 
 		EnableAgentPidNs: runtimeConfig.EnableAgentPidNs,
 
 		DisableGuestSeccomp: runtimeConfig.DisableGuestSeccomp,
+
+		VFIOInGuest: runtimeConfig.VFIOInGuest,
 
 		// Q: Is this really necessary? @weizhang555
 		// Spec: &ocispec,